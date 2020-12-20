3eaf02debe9f12bdc0caddf6190847821e01ce82
wecassidy <wec.notif.github@gmail.com>
2020-12-20 16:27:48 -0500

    Allow language and exec to be passed by CLI

--- runmd.py
+++ runmd.py
@@ -7,7 +7,14 @@ import tempfile
 
 from markdown_it import MarkdownIt
 
-__version__ = "0.1"
+__version__ = "0.2"
+
+
+def build_command(command, name):
+    """
+    Insert a filename into an executable command where appropriate.
+    """
+    return command + " " + name
 
 
 def runmd(text, language, command):
@@ -20,7 +27,9 @@ def runmd(text, language, command):
     with tempfile.NamedTemporaryFile() as fp:
         fp.write(code.encode("UTF-8"))
         fp.seek(0)
-        result = subprocess.run(command + [fp.name], check=False)
+        result = subprocess.run(
+            build_command(command, fp.name), check=False, shell=True
+        )
 
     return result
 
@@ -30,9 +39,14 @@ if __name__ == "__main__":
         description="Concatenate tagged Markdown code fences and execute the result."
     )
     parser.add_argument("file", help="Markdown file to parse")
+    parser.add_argument(
+        "-l", "--lang", metavar=("language", "command"), nargs=2, help="blah"
+    )
     args = parser.parse_args()
 
     with open(args.file) as f:
         text = f.read()
 
-    sys.exit(runmd(text, "python", ["python3"]).returncode)
+    lang = args.lang if args.lang is not None else ("python", "python3")
+
+    sys.exit(runmd(text, *lang).returncode)
