ae1a42e4e96ed25cde2ca2c61fe78dfe1aa2ea86
wecassidy <wec.notif.github@gmail.com>
2020-12-20 14:54:45 -0500

    Read Python blocks from MD files

--- runmd.py
+++ runmd.py
@@ -2,5 +2,23 @@
 
 import argparse
 
+from markdown_it import MarkdownIt
+
+__version__ = "0.1"
+
 if __name__ == "__main__":
-    pass
+    parser = argparse.ArgumentParser(
+        description="Concatenate tagged Markdown code fences and execute the result."
+    )
+    parser.add_argument("file", help="Markdown file to parse")
+    args = parser.parse_args()
+
+    with open(args.file) as f:
+        text = f.read()
+
+    md = MarkdownIt("commonmark")
+    tokens = md.parse(text)
+    code = "".join(
+        t.content for t in tokens if t.type == "fence" and t.info.lower() == "python"
+    )
+    print(code)
